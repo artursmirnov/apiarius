baf78627efb49905fbb38b7765b27ec6915c5e41
Artur Smirnov <mail@artursmirnov.com>
2015-11-17 17:03:25 +0300

    Set up ember testing

---
+++ client/tests/acceptance/sanity-test.js
@@ -0,0 +1,26 @@
+/* jshint expr:true */
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+import { expect } from 'chai';
+import startApp from '../helpers/start-app';
+import Ember from 'ember';
+
+describe('Acceptance: Sanity', function() {
+  var application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    Ember.run(application, 'destroy');
+  });
+
+  it('can run tests', function() {
+    expect(true).to.be.true;
+  });
+});
