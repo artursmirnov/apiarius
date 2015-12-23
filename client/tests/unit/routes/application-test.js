ee8ed759a604ba2611cc2ea86da8508ceaaf0036
Artur <mail@artursmirnov.com>
2015-12-23 20:05:09 +0300

    Fix test

--- client/tests/unit/routes/application-test.js
+++ client/tests/unit/routes/application-test.js
@@ -11,6 +11,7 @@ describeModule(
   {
     // Specify the other units that are required for this test.
     // needs: ['controller:foo']
+    needs: ['service:session']
   },
   function() {
     it('exists', function() {
