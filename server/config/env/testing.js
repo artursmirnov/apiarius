4bbf3e9a6ad2d69e5990330aa78b83a0eefd59c7
Artur <mail@artursmirnov.com>
2015-12-23 23:35:20 +0300

    Switch test connection to diskDb

---
+++ server/config/env/testing.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  github: {
+    callback_url: '/auth/github_cb', // overridden in ./env/*,
+    client_id: '123', // defaults for tests
+    client_secret: '123', // defaults for tests
+    scope: ['user', 'repo', 'write:repo_hook']
+  },
+
+  models: {
+    connection: 'localDiskDb' // check connection name
+  }
+
+};
